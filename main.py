b2fe7ca710de4ba194222f5fd980690489d7c454
Alfie Barlow <alfiewbarlow@gmail.com>
2022-02-22 10:25:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #from pyfiglet import figlet_format
+#import pyfiglet.fonts
 import argparse
 import json
 import os
@@ -25,8 +26,10 @@ if args.setup:
     name = input("Name > ")
     version = input("Version > ")
     architecture = input("Architecture > ")
+    maintainer = input("Maintainer > ")
     with open("debassistant.json", "w") as f:
-        f.write(json.dumps({"name": name, "version": version, "architexture": architecture, "debassistant": {"version": VERSION}}))
+        f.write(json.dumps({"name": name, "version": version, "architecture": architecture, "debassistant": {"version": VERSION}}))
+        f.close()
     #print(figlet_format("Done!"))
     print("Done!")
     print("Run --make to expand config!")
@@ -40,6 +43,19 @@ elif args.make:
     os.mkdir(f"{data['name']}/DEBIAN")
     print(f"Created {data['name']}/DEBIAN")
 
+    with open(f"{data['name']}/DEBIAN/control", "w") as f:
+        f.write(f"""Package: {data["name"]}
+        Version: {data["version"]}
+        Maintainer: {data["maintainer"]}
+        Architecture: {data["architecture"]}
+        Description: {data["name"]} created by DebAssistant
+        """)
+        f.close()
+    
+    with open(f"{data['name']}/DEBIAN/postinst", "w") as f:
+        f.write("echo Installation complete - DebAssistant")
+        f.close()
+
     #print(figlet_format("Done!"))
     print("Done!")
     print("Edit DEBIAN/postinst to edit commands ran after the install of the files.")
@@ -48,6 +64,7 @@ elif args.make:
 elif args.build:
     with open("debassistant.json", "r") as f:
         data = json.loads(f.read())
+        f.close()
     #print(figlet_format("Building"))
     print("Building")
     res = subprocess.run(f"dpkg -b {data['name']}", shell=True, stdout=subprocess.PIPE)
